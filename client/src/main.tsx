d9836cc2de221faa2d08719ea83c04188eff20bc
jessl314 <jlchrysanthemum456@gmail.com>
2026-06-05 21:47:39 -0700

    finished client-side-rendering

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import CodeEditor from './components/CodeEditor.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
-    <CodeEditor/>
-
+    <App />
   </StrictMode>,
 )
