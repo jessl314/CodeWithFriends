8c6ff863c2565aef8c4b8be3c220d4d706da4214
jessl314 <jlchrysanthemum456@gmail.com>
2026-06-20 14:22:39 -0700

    set up basic websocket and message broker

--- server/src/main/java/com/codewithfriends/config/WebSocketConfig.java
+++ server/src/main/java/com/codewithfriends/config/WebSocketConfig.java
@@ -1,7 +1,34 @@
-package com.codewithfriends.config
+package com.codewithfriends.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/*
+message broker translates, routes and distributes messages betweeen different applications or services
+
+-> achieves decoupling in a Publisher/Subscriber architecture so the sender (Pub) does not need to know who is recieving the message, how many, or where they are located. The receiver (Sub) tells the broker what data they are intereted in to later send to the Pub
+
+*/
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // set up message broker for public broadcasts
+        config.enableSimpleBroker("/topic");
+        // incoming messages from React that hit controllers will use this prefix
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // URL handshake endpoint for React client to connect to
+        registry.addEndpoint("/cwf-edit")
+                .setAllowedOriginPatterns("*").withSockJS();
+    }
+
+}
